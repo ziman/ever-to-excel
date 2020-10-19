27cfa6df28e726f2e17944547bd1fd8c50972276
Matus Tejiscak <ziman@functor.sk>
2020-10-19 22:20:20 +0200

    Add Lua parser.

---
+++ example/hello-world.lua
@@ -0,0 +1,3 @@
+x = "hello world"
+y = 1
+y = y + 1
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,11 @@
 module Main where
 
+import Language.Lua
+import qualified Data.Text.IO as Text
+
 main :: IO ()
-main = putStrLn "hello world"
+main = do
+  input <- Text.getContents
+  case parseText chunk input of
+    Left (rng, msg) -> error $ msg ++ " at " ++ show rng
+    Right block -> print $ pprint block
