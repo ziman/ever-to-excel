26fe912870a359a4c3012176747c28117ee65ace
Matus Tejiscak <ziman@functor.sk>
2020-10-20 02:01:59 +0200

    Implement 1+2.

--- src/SCompile.hs
+++ src/SCompile.hs
@@ -18,54 +18,37 @@ data State = State
 
 type CG = RWST () SCode State (Except String)
 
-{-
-lookup :: String -> Env -> Either String Addr
-lookup var [] = Left $ "unknown variable: " ++ show var
-lookup var (x:xs)
-  | var == x  = pure $ Addr (1 + length xs)
-  | otherwise = lookup var xs
--}
-
 throw :: String -> CG a
 throw = lift . throwE
 
 emit :: SInstr -> CG ()
 emit instr = tell [instr]
 
-top :: Int -> CG Addr
-top i = do
-  st <- get
-  return (Addr $ stStackSize st - i)
-
 {-
-alloc :: Int -> CG ()
-alloc n = do
-  st <- get
-  put st{ stStackSize = stStackSize st + n }
--}
+top :: Int -> CG ()
+top 0 = do
+  emit $ LOAD addrSP
 
-free :: Int -> CG ()
-free n = do
-  st <- get
-  if stStackSize st < n
-    then throw "stack underflow"
-    else put st{ stStackSize = stStackSize st - n }
+top i = do
+  emit $ LOAD addrSP
+  emit $ PUSHI i
+  emit $ BIN SUB
+-}
 
 compileExpr :: RichSExpr Atom -> CG ()
 
+compileExpr (RSAtom (Number i)) = do
+  emit $ PUSHI i
+
 compileExpr (RSList [RSAtom (Symbol "display"), xe]) = do
   compileExpr xe
-  xaddr <- top 0
-  emit $ MOV output xaddr
+  emit $ PRINT
   -- returns the printed value
 
 compileExpr (RSList [RSAtom (Symbol "+"), xe, ye]) = do
   compileExpr xe
   compileExpr ye
-  xaddr <- top 1
-  yaddr <- top 0
-  emit $ BIN ADD xaddr xaddr yaddr
-  free 1
+  emit $ BIN ADD
 
 compileExpr e = throw $ "can't compile: " ++ show e
 
--- src/SLang.hs
+++ src/SLang.hs
@@ -11,16 +11,21 @@ data Bin
   deriving (Eq, Ord, Show)
 
 data SInstr
-  = MOV Addr Addr
-  | BIN Bin Addr Addr Addr
-  | PEEK Addr Addr
-  | POKE Addr Addr
-  | WSTR Addr String
-  | WINT Addr Int
-  | WFLT Addr Double
+  = LOAD Addr
+  | STORE Addr
+  | BIN Bin
+  | PUSHS String
+  | PUSHI Int
+  | PRINT
   deriving (Eq, Ord, Show)
 
 type SCode = [SInstr]
 
-output :: Addr
-output = Addr 0
+addrOUT :: Addr
+addrOUT = Addr 0
+
+addrPC :: Addr
+addrPC = Addr 1
+
+addrSP :: Addr
+addrSP = Addr 2
