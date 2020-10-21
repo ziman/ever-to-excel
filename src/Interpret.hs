8fa50c6f223cb36f0789f535a6f40755c05b4c7c
Matus Tejiscak <ziman@functor.sk>
2020-10-21 22:12:43 +0200

    More interpreter.

--- src/Interpret.hs
+++ src/Interpret.hs
@@ -1,7 +1,11 @@
 module Interpret (Cell(..), run) where
 
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as IntMap
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.RWS.CPS
 
 import Bytecode
 
@@ -10,24 +14,58 @@ data Cell
   | Str String
   deriving (Eq, Ord, Show)
 
-loop :: IntMap (Instr PC) -> IntMap Cell -> [Cell] -> Either String [Cell]
-loop code mem acc
-  | Just (Int pc) <- IntMap.lookup 0 mem
-  , Just instr <- IntMap.lookup pc code
-  = case instr of
-      HALT -> pure acc
-      _    -> Left $ "not implemented: " ++ show instr
+type Exec = RWST (Map PC (Instr PC)) [Cell] (Map Addr Cell) (Except String)
 
-  | otherwise = Left "wrong PC"
+eval :: XExpr -> Exec Cell
+eval e = throw $ "not implemented: eval " ++ show e
+
+peek :: Addr -> Exec Cell
+peek addr = do
+  mem <- get
+  case Map.lookup addr mem of
+    Nothing -> throw $ "could not read " ++ show addr
+    Just cell -> pure cell
+
+getPC :: Exec PC
+getPC = peek addrPC >>= \case
+  Int i -> pure (PC i)
+  cell  -> throw $ "could not load PC: " ++ show cell
+
+getInstr :: Exec (Instr PC)
+getInstr = do
+  pc <- getPC
+  code <- ask
+  case Map.lookup pc code of
+    Nothing -> throw $ "no instruction at " ++ show pc
+    Just instr -> pure instr
+
+throw :: String -> Exec a
+throw = lift . throwE
+
+{-
+emit :: Cell -> Exec ()
+emit cell = tell [cell]
+-}
+
+loop :: Exec ()
+loop = getInstr >>= \case
+  HALT -> pure ()
+  OP _n e -> do
+    _ev <- eval e
+    pure ()
+  instr -> throw $ "not implemented: " ++ show instr
 
 run :: Code PC -> Either String [Cell]
-run code = loop instrs initialMemory []
+run code =
+  fmap snd $
+    runExcept $
+      evalRWST loop instrs mem
   where
-    instrs = IntMap.fromList [(pc, instr) | (pc, instr) <- zip [0..] code]
-    initialMemory = IntMap.fromList
-      [ (0, Int 0)  -- OUT
-      , (1, Int 0)  -- PC
-      , (2, Int 4)  -- SP
-      , (3, Int 4)  -- BP
+    instrs = Map.fromList [(PC pc, instr) | (pc, instr) <- zip [0..] code]
+    mem = Map.fromList
+      [ (addrOUT, Int 0)  -- 0
+      , (addrPC,  Int 0)  -- 1
+      , (addrSP,  Int 4)  -- 2
+      , (addrBP,  Int 4)  -- 3
       ]
 
